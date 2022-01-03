f879d8945c07642c94e00a135fe0274734658936
Stanley <stang0926@gmail.com>
2022-01-03 17:25:55 -0500

    export game executable

--- OpenGL_Project/src/Main.cpp
+++ OpenGL_Project/src/Main.cpp
@@ -1,10 +1,10 @@
 #include"engine/Game.h"
 
-// CURRENT LINES OF CODE: 5305
+// CURRENT LINES OF CODE: 5348
 
 int main()
 {
-	Game* game = new Game("OpenGL Project", 1270, 800, 4, 6, GL_FALSE);
+	Game* game = new Game("The Floor is Lava", 1270, 800, 4, 6, GL_FALSE);
 	
 	// MAIN LOOP
 	while (!game->get_window_should_close())
